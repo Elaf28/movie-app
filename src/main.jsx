91f5aaab3557333a956451fb1126525c01e11b85
elaf <elaf6369@gmail.com>
2026-04-22 21:37:43 +0200

    setup react-router and define authentication routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './App.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import MainLayout from './layouts/MainLayout';
 import NotFound404 from './pages/NotFound404';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +19,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <App />,
+      },
+      {
+        path: 'register',
+        element: <Register/>,
+      },
+      {
+        path: 'login',
+        element: <Login/>,
       },
     ],
   },
