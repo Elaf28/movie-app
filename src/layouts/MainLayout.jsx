bf7b9afb440e953180b2922967cf5272c6c5c0c8
elaf <elaf6369@gmail.com>
2026-04-24 13:58:17 +0300

    add FavoritesList and Watchlist components with routing support

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,24 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import React from 'react';
+import { useMovieStore } from '@/Store/zustand/useMovieStore';
+import React, { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router';
 
 const MainLayout = () => {
+    const sync = useMovieStore((state) => state.syncWithUser);
+    useEffect(() => {
+        sync();
+    }, [sync]);
     return (
         <>
-        <div className='min-h-screen flex flex-col bg-slate-50'>
+        <div className='min-h-screen flex flex-col'>
             <Header />
             <main >
                 <Outlet />
             </main>
             <Footer />
+            <Toaster position="top-right" />
         </div>
         </>
     );
