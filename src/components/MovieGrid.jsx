0c35188e73008ec2f368756e1fc5f42c09199039
Bemin Raafat <beminraafat@gmail.com>
2026-04-26 04:57:08 +0300

    feat: add movie card skeleton, genre card skeleton, and isLoading to discover movies page

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import MovieCardSkeleton from './MovieCardSkeleton';
 
 function MovieGrid({ movies, isLoading }) {
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 mt-8">
+        {[...Array(20)].map((_, i) => (
+          <MovieCardSkeleton key={i} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 mt-8">
       {movies.map((movie) => (
