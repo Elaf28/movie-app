ade7d58266285992020ce915a661860026582de2
elaf <elaf6369@gmail.com>
2026-04-29 02:12:24 +0300

    add ThemeToggle component and integrate it into Login and Register pages

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,33 @@
+import { HiSun, HiMoon } from "react-icons/hi";
+import { useState, useEffect } from "react";
+
+const ThemeToggle = () => {
+    const [darkMode, setDarkMode] = useState(
+        localStorage.getItem("theme") !== "light"
+    );
+
+    useEffect(() => {
+        if (darkMode) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+        } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        }
+    }, [darkMode]);
+
+    return (
+        <button
+        onClick={() => setDarkMode(!darkMode)}
+        className="text-4xl md:text-5xl hover:text-primary transition cursor-pointer bg-card/50 backdrop-blur-md p-2 rounded-full shadow-md"
+        >
+        {darkMode ? (
+            <HiSun className="hover:text-yellow-500" />
+        ) : (
+            <HiMoon />
+        )}
+        </button>
+    );
+};
+
+export default ThemeToggle;
